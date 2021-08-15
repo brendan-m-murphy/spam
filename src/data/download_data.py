7b45d2def33b515fcd15f83841cdf85c21c23f8b
Brendan Murphy <git@mellotron.anonaddy.com>
2021-08-15 17:01:02 +0100

    Added comments and main() to download.py

--- src/data/download_data.py
+++ src/data/download_data.py
@@ -10,6 +10,10 @@ REF_PATH = os.path.join('..', '..', 'references')
 
 
 def download_data():
+    """Fetch links for spam and ham emails, download readme,
+    download and unzip emails.
+
+    """
     DL_ROOT = 'https://spamassassin.apache.org/old/publiccorpus/'
 
     # get file names
@@ -36,7 +40,7 @@ def download_data():
         DL_URL = DL_ROOT + file_name
         response = requests.get(DL_URL)
         with open(bz2_path, 'wb') as f:
-            f.write(r.content)
+            f.write(response.content)
             file_bz2 = tarfile.open(bz2_path)
             file_bz2.extractall(path=DATA_PATH)
             file_bz2.close()
@@ -47,7 +51,21 @@ def download_data():
 
 
 def clean_up():
+    """
+    Remove by-products of file downloads.
+
+    """
+
     for file in glob.glob(os.path.join(DATA_PATH, '*.bz2')):
         os.remove(file)
     for file in glob.glob(os.path.join(REF_PATH, '*.bz2')):
         os.remove(file)
+
+
+def main():
+    download_data()
+    clean_up()
+
+
+if __name__='__main__':
+    main()
